b798cd91e2f295f2249fe321d42350f844341325
Ruslan Koshevoy <ruslan.koshevoj@gmail.com>
2018-04-12 13:59:02 +0300

    version 0.1 alpha
    - added favicon;
    - optimized code;
    - setting line width;
    - added font awesome;
    - clearing canvas;
    - other minor upodates;

--- js/main.js
+++ js/main.js
@@ -1,12 +1,69 @@
-var colors = document.getElementsByClassName('color'),
-    blackColor = document.getElementById('black'),
-    whiteColor = document.getElementById('white'),
-    greyColor = document.getElementById('grey'),
-    redColor = document.getElementById('red'),
-    greenColor = document.getElementById('green'),
-    blueColor = document.getElementById('blue'),
-    yellowColor = document.getElementById('yellow'),
-    lineColor = '#000';
+var lineWidthItems = document.getElementsByClassName('line-width'),
+    lineWidth = '2',
+    colors = document.getElementsByClassName('color'),
+    lineColor = '#000',
+    clearCanvas = document.getElementById('clear-canvas'),
+
+    canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d'),
+    isMouseDown = false;
+
+// setting size of canvas
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// setting line width
+
+for (i = 0; i < lineWidthItems.length; i++) {
+  lineWidthItems[i].addEventListener('click', function() {
+
+    for (i = 0; i < lineWidthItems.length; i++) {
+      lineWidthItems[i].classList.remove('line-width-active')
+    };
+
+    this.classList.add('line-width-active');
+
+    switch(this.id) {
+      case 'line-width-2': {
+        lineWidth = '2';
+        painting();
+        console.log(lineWidth);
+        break;
+      }
+      case 'line-width-4': {
+        lineWidth = '4';
+        painting();
+        console.log(lineWidth);
+        break;
+      }
+      case 'line-width-6': {
+        lineWidth = '6';
+        painting();
+        console.log(lineWidth);
+        break;
+      }
+      case 'line-width-10': {
+        lineWidth = '10';
+        painting();
+        console.log(lineWidth);
+        break;
+      }
+      case 'line-width-20': {
+        lineWidth = '20';
+        painting();
+        console.log(lineWidth);
+        break;
+      }
+      default: {
+        lineWidth = '2px';
+        painting();
+      }
+    }
+  });
+};
+
+// setting color
 
 for (i = 0; i < colors.length; i++) {
   colors[i].addEventListener('click', function() {
@@ -17,51 +74,70 @@ for (i = 0; i < colors.length; i++) {
 
     this.classList.add('active-color');
 
-    if (this.classList.contains('black')) {
-      lineColor = '#000';
-      painting();
-    } else if (this.classList.contains('white')) {
-      lineColor = '#fff';
-      painting();
-    } else if (this.classList.contains('grey')) {
-      lineColor = '#ccc';
-      painting();
-    } else if (this.classList.contains('red')) {
-      lineColor = '#ff0000';
-      painting();
-    } else if (this.classList.contains('green')) {
-      lineColor = '#008000';
-      painting();
-    } else if (this.classList.contains('blue')) {
-      lineColor = '#0000ff';
-      painting();
-    } else if (this.classList.contains('yellow')) {
-      lineColor = '#ffff00';
-      painting();
+    switch(this.id) {
+      case 'black': {
+        lineColor = '#000';
+        painting();
+        break;
+      }
+      case 'white': {
+        lineColor = '#fff';
+        painting();
+        break;
+      }
+      case 'grey': {
+        lineColor = '#ccc';
+        painting();
+        break;
+      }
+      case 'red': {
+        lineColor = '#ff0000';
+        painting();
+        break;
+      }
+      case 'green': {
+        lineColor = '#008000';
+        painting();
+        break;
+      }
+      case 'blue': {
+        lineColor = '#0000ff';
+        painting();
+        break;
+      }
+      case 'yellow': {
+        lineColor = '#ffff00';
+        painting();
+        break;
+      }
+      default: {
+        lineColor = '#000';
+        painting();
+      }
     }
   });
-}
+};
 
-var canvas = document.getElementById('canvas'),
-  ctx = canvas.getContext('2d'),
-  isMouseDown = false;
+// clearing canvas
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+clearCanvas.addEventListener('click', function(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
 
 canvas.addEventListener('mousedown', function(e) {
   isMouseDown = true;
 });
-
 canvas.addEventListener('mouseup', function() {
   isMouseDown = false;
   ctx.beginPath();
 });
 
+// painting
+
 function painting() {
   ctx.fillStyle = lineColor;
   ctx.strokeStyle = lineColor;
-  ctx.lineWidth = 10;
+  ctx.lineWidth = lineWidth;
   canvas.addEventListener('mousemove', function(e) {
 
     if (isMouseDown) {
@@ -70,7 +146,7 @@ function painting() {
       ctx.stroke();
 
       ctx.beginPath();
-      ctx.arc(e.clientX, e.clientY, 5, 0, Math.PI * 2);
+      ctx.arc(e.clientX, e.clientY, lineWidth / 2, 0, Math.PI * 2);
       ctx.fill();
 
       ctx.beginPath();
